897173332967f89d65c0a858ad2d68b36c6adb21
44x1carbon <b4079@oic.jp>
2015-12-24 21:43:45 -0800

    メンバーにオーダーズ追加

--- member.php
+++ member.php
@@ -18,4 +18,14 @@ class Model_Member extends \Orm\Model
 
 	protected static $_table_name = 'tbl_member';
 
+	protected static $_has_many = array(
+		'orders' => array(
+			'model_to' => 'Model_Order',
+			'key_from' => 'id',
+			'key_to' => 'member_id',
+			'cascade_save' => true,
+			'cascade_delete' => false,
+		)
+	);
+
 }
